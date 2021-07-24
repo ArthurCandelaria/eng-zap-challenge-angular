45ff8bfe92d4268561eb3531ae6418a645d28471
Arthur Candelaria <arthur.candelaria@live.com>
2021-07-24 00:55:47 -0300

    get filters

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,6 +12,12 @@ export class HomeComponent implements OnInit {
   page = 1;
   isList: boolean;
   plataform: string;
+  boundingBox = [
+    { 'minlon': -46.693419 },
+    { 'minlat': -23.568704 },
+    { 'maxlon': -46.641146 },
+    { 'maxlat': -23.546686 }
+  ]
 
   constructor(
     private grupozapService: GrupozapService
@@ -41,31 +47,82 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getImmobles();
-    this.blockButtom();
-    this.imgFormater();
   }
 
   getImmobles() {
     this.grupozapService.immobileList().subscribe(
       success => {
+
         const res: any = success;
-        if (this.plataform.toLocaleLowerCase() === 'zap') {
-          const saleZap = res.filter((immobile: any) => immobile.pricingInfos.businessType === 'SALE' && immobile.pricingInfos.price >= 600000);
-          const rentalZap = res.filter((immobile: any) => immobile.pricingInfos.businessType === 'RENTAL' && immobile.pricingInfos.rentalTotalPrice >= 3500);
-          this.immobileList = [...saleZap, ...rentalZap];
-        } else if (this.plataform.toLocaleLowerCase() === 'vivareal') {
-          const saleVivareal = res.filter((immobile: any) => immobile.pricingInfos.businessType === 'SALE' && immobile.pricingInfos.price <= 700000);
-          const rentalVivareal = res.filter((immobile: any) => immobile.pricingInfos.businessType === 'RENTAL' && immobile.pricingInfos.rentalTotalPrice <= 4000);
-          this.immobileList = [...saleVivareal, ...rentalVivareal];
+
+        if (localStorage.getItem('plataform') === 'zap') {
+          const saleZap = res.filter((immobile: any) => {
+            return immobile.pricingInfos.businessType === 'SALE'
+              && immobile.pricingInfos.price >= 600000
+              && immobile.address.geoLocation.location.lat != 0
+              && immobile.address.geoLocation.location.lon != 0
+              && immobile.usableAreas > 0
+              && (immobile.pricingInfos.price / immobile.pricingInfos.price) > 3500
+          });
+          const saleZap2 = saleZap.filter((immobile: any) => {
+            return immobile.address.geoLocation.location.lon >= this.boundingBox[0] 
+            && immobile.address.geoLocation.location.lon <= this.boundingBox[2]
+            && immobile.address.geoLocation.location.lat >= this.boundingBox[1] 
+            && immobile.address.geoLocation.location.lat <= this.boundingBox[3]
+            && (immobile.pricingInfos.price / immobile.pricingInfos.price) > 3850
+          });
+
+          const rentalZap = res.filter((immobile: any) => {
+            return immobile.pricingInfos.businessType === 'RENTAL'
+              && immobile.pricingInfos.rentalTotalPrice >= 3500
+              && immobile.address.geoLocation.location.lat != 0
+              && immobile.address.geoLocation.location.lon != 0
+          });
+
+          this.immobileList = [...saleZap2, ...rentalZap];
+
+        } else if (localStorage.getItem('plataform') === 'vivareal') {
+          const saleVivareal = res.filter((immobile: any) => {
+            return immobile.pricingInfos.businessType === 'SALE'
+              && immobile.pricingInfos.price <= 700000
+              && immobile.address.geoLocation.location.lat != 0
+              && immobile.address.geoLocation.location.lon != 0
+          });
+          const rentalVivareal = res.filter((immobile: any) => {
+            return immobile.pricingInfos.businessType === 'RENTAL'
+              && immobile.pricingInfos.rentalTotalPrice <= 4000
+              && immobile.address.geoLocation.location.lat != 0
+              && immobile.address.geoLocation.location.lon != 0
+              && immobile.pricingInfos.monthlyCondoFee != 0
+              && immobile.pricingInfos.monthlyCondoFee != ''
+              && (immobile.pricingInfos.monthlyCondoFee < (immobile.pricingInfos.rentalTotalPrice * 0.3))
+          });
+
+          const rentalVivareal2 = rentalVivareal.filter((immobile: any) => {
+            return immobile.address.geoLocation.location.lon >= this.boundingBox[0] 
+            && immobile.address.geoLocation.location.lon <= this.boundingBox[2]
+            && immobile.address.geoLocation.location.lat >= this.boundingBox[1] 
+            && immobile.address.geoLocation.location.lat <= this.boundingBox[3]
+            && (immobile.pricingInfos.monthlyCondoFee < (immobile.pricingInfos.rentalTotalPrice * 0.5))
+          });
+
+          this.immobileList = [...saleVivareal, ...rentalVivareal2];
         }
       }, error => {
         // console.warn(error);
       }
     );
+
+    this.blockButtom();
+    this.imgFormater();
+
   }
 
   reciverFeedback(event: any) {
     localStorage.setItem('plataform', event);
+    this.page = 1
+    this.immobileList = [];
+    this.getImmobles();
   }
 
   blockButtom() {
